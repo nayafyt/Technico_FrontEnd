b6fab67e5c83af1e8e20e86f8c9ba175820206f7
Angelos Osafi <aggelososafi11@gmail.com>
2024-12-28 14:55:01 +0200

    changed structure to router-outlet

--- TechnicoUI/src/app/pages/admin-homepage/admin-homepage.component.ts
+++ TechnicoUI/src/app/pages/admin-homepage/admin-homepage.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../shared/footer/footer.component';
 import { NavbarComponent } from '../shared/navbar/navbar.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-admin-homepage',
   standalone: true,
-  imports: [FooterComponent, NavbarComponent],
+  imports: [FooterComponent, NavbarComponent, RouterOutlet],
   templateUrl: './admin-homepage.component.html',
-  styleUrl: './admin-homepage.component.scss'
+  styleUrl: './admin-homepage.component.scss',
 })
-export class AdminHomepageComponent {
-
-}
+export class AdminHomepageComponent {}
