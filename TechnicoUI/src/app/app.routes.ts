832d8e5beb9a8b89d95c7d785c8ebebe31a5e662
Angelos Osafi <aggelososafi11@gmail.com>
2024-12-28 14:54:14 +0200

    add routes config

--- TechnicoUI/src/app/app.routes.ts
+++ TechnicoUI/src/app/app.routes.ts
@@ -1,9 +1,22 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 import { AdminHomepageComponent } from './pages/admin-homepage/admin-homepage.component';
+import { RepairsComponent } from './pages/repairs/repairs.component';
+import { PropertyOwnersComponent } from './pages/property-owners/property-owners.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-    {
-        path:'admin-homepage',
-        component:AdminHomepageComponent
-    }
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'admin-homepage',
+    component: AdminHomepageComponent,
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path: 'repairs', component: RepairsComponent },
+      { path: 'property-owners', component: PropertyOwnersComponent },
+    ],
+  },
+  { path: '**', redirectTo: 'login' },
 ];
