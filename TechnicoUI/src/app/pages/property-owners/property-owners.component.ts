1fc71ab5da152f8958cf65e79075d8acce8c0bbe
Angelos Osafi <aggelososafi11@gmail.com>
2024-12-28 14:51:10 +0200

    add home component repairs component property-owner component

---
+++ TechnicoUI/src/app/pages/property-owners/property-owners.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-property-owners',
+  standalone: true,
+  imports: [],
+  templateUrl: './property-owners.component.html',
+  styleUrl: './property-owners.component.scss'
+})
+export class PropertyOwnersComponent {
+
+}
