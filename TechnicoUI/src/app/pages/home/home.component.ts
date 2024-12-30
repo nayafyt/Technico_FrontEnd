0b18c6de7eb5335992dd67819c4981e850f0567d
Christina Galani <xristinagalani98@gmail.com>
2024-12-30 18:48:33 +0200

    add Admin-Homepage css to HomeComponent.css

--- TechnicoUI/src/app/pages/home/home.component.ts
+++ TechnicoUI/src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
