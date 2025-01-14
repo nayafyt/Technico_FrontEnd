03299779c34f539ddbb6d2e741725e2ff0340088
Angelos Osafi <aggelososafi11@gmail.com>
2025-01-14 08:04:12 +0200

    change repairs interface and add api response interfaces

--- TechnicoUI/src/app/models/irepairs.ts
+++ TechnicoUI/src/app/models/irepairs.ts
@@ -1,13 +1,41 @@
 export interface IRepairs {
-    date: string;
-    repairType: string;
-    description: string;
+  id?:number
+  dateTime: string;
+  repairType: string;
+  description?: string;
+  address: string;
+  status: string;
+  cost: number;
+  propertyOwnerDto: {
+    vatNumber: number;
     address: string;
-    status: 'in progress'|'pending'|'declined';
-    cost: number;
-    owner: string;
-    imageUrl?: string;
-    title: string;
-    location: string;
-    userId: string;
+    name: string;
+    surname: string;
+    phoneNumber: string;
+    email: string;
+    password: string;
+    userType: string;
+    propertyItems: [
+      {
+        propertyIdentificationNumber: string;
+        yearOfConstruction: number;
+        address: string;
+        propertyType: string;
+        propertyOwnerVatNumber: string;
+      }
+    ];
+    propertyRepairs: [];
+  };
+}
+
+export interface ApiResponse {
+  value: IRepairs;
+  statusCode: number;
+  description: string;
+}
+
+export interface ApiResponse2 {
+  value: IRepairs[];
+  statusCode: number;
+  description: string;
 }
