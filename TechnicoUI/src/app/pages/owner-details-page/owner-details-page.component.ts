15fb386dd7057211d3aebac2fad431fd03b6b8c7
NayaF <nfytali@gmail.com>
2025-01-15 14:46:49 +0200

    Add info button on owner-page

---
+++ TechnicoUI/src/app/pages/owner-details-page/owner-details-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-owner-details-page',
+  imports: [],
+  templateUrl: './owner-details-page.component.html',
+  styleUrl: './owner-details-page.component.scss'
+})
+export class OwnerDetailsPageComponent {
+
+}
